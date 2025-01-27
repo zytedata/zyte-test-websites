a0e3a42931561da6dc808ceffca0a192bb469e1f
Andrey Rakhmatullin <wrar@wrar.name>
2025-01-27 21:07:22 +0500

    Basic pagination.

--- zyte_test_websites/jobs/views.py
+++ zyte_test_websites/jobs/views.py
@@ -11,15 +11,24 @@ from .items import JobsDataKey
 routes = web.RouteTableDef()
 
 
+CATS_PER_PAGE = 10
+JOBS_PER_PAGE = 10
+
+
 @routes.get("/")
 @aiohttp_jinja2.template("index.jinja2")
 async def index(request: web.Request) -> dict[str, Any]:
     """Home page with a list of categories."""
-    # TODO: pagination
     categories = request.app[JobsDataKey].categories
+    page = int(request.query.get("page", 1))
     return {
-        "categories": sorted(categories.values(), key=operator.attrgetter("id")),
+        "categories": sorted(categories.values(), key=operator.attrgetter("id"))[
+            CATS_PER_PAGE * (page - 1) : CATS_PER_PAGE * page
+        ],
+        "current_page": page,
+        "total_pages": len(categories) // CATS_PER_PAGE + 1,
         "total_categories": len(categories),
+        "base_url": request.path,
     }
 
 
@@ -27,14 +36,21 @@ async def index(request: web.Request) -> dict[str, Any]:
 @aiohttp_jinja2.template("job_list.jinja2")
 async def job_list(request: web.Request) -> dict[str, Any]:
     """A page with a list of job postings"""
-    # TODO: pagination
     category = request.app[JobsDataKey].categories[
         int(request.match_info["category_id"])
     ]
+    page = int(request.query.get("page", 1))
     return {
         "category": category,
-        "jobs": category.jobs.values(),
+        "jobs": sorted(
+            category.jobs.values(),
+            key=operator.attrgetter("date_published"),
+            reverse=True,
+        )[JOBS_PER_PAGE * (page - 1) : JOBS_PER_PAGE * page],
+        "current_page": page,
+        "total_pages": len(category.jobs) // JOBS_PER_PAGE + 1,
         "total_jobs": len(category.jobs),
+        "base_url": request.path,
     }
 
 
